d19b13cd86c9de27931992105bb33ea94344fc33
ayoungprogrammer <michaelyoung1995@gmail.com>
2014-03-15 18:36:38 -0400

    init mysql

---
+++ db.php
@@ -0,0 +1,14 @@
+<php?
+
+$username="root";
+$password="";
+$database="wikialg";
+
+mysql_connect(localhost,$username,$password);
+
+@mysql_select_db($database) or die( "Unable to select database");
+
+mysql_close();
+
+
+?>
