86627ccc10ee5915f4e420d1b7f05d1f032e4825
simonhuang989 <simonhuang989@hotmail.com>
2014-03-20 14:32:04 -0400

    added about content

--- templates/main.html.php
+++ templates/main.html.php
@@ -16,30 +16,36 @@
 <section class="info container">
 
     <main>
-        <article>
-        <h3>Computer Science Algorithms</h3>
-        <hr/>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat ullamcorper justo ac tincidunt.
-            Cras blandit mauris sapien, quis laoreet leo mattis vel. Etiam ac metus blandit, eleifend magna eu,
-            molestie mauris. Ut viverra enim tortor, sed ullamcorper nunc sollicitudin id. Donec sollicitudin
-            pretium nisl, vitae semper mi hendrerit id. Aenean convallis ante a elit ullamcorper malesuada.
-            Curabitur a dui at metus porta fringilla ac sed odio.
-        </p>
-        <p>
-            Curabitur ullamcorper elementum urna ut volutpat. Vivamus scelerisque lorem a aliquet adipiscing.
-            Aenean aliquam lobortis est. Etiam posuere gravida mi nec ultrices. Duis leo sem, interdum sit amet
-            magna in, placerat elementum nibh. Vestibulum sit amet adipiscing lorem, quis blandit risus. Integer
-            in facilisis purus. Donec fermentum venenatis consequat. Sed non auctor diam. Morbi sed consequat enim.
-            Nunc sed rhoncus lectus. Nulla blandit libero a ligula rutrum imperdiet. Donec varius pulvinar mauris,
-            eget ultrices tellus suscipit eu.
-        </p>
+       <article>
+            <h2>The Computer Science Handbook</h2>
+            <hr/>
+            <p>
+                Have you ever had trouble searching for a compiled library of the most common computer science algorithms?
+                Maybe it was to supplement material taught in a programming course. Maybe it was to prepare for a
+                programming contest or a technical interview. Or maybe you were just nerdy and awesome enough to want to
+                learn about some CS algorithms in your free time. If any of that sounds like you, then welcome to The
+                CS Handbook.
+            </p>
+            <p>
+                The CS Handbook is an on-going project started by two computer science students from the University of
+                Waterloo. It's a collection of computer science algorithms that ranges from simple calculations such as the
+                GCD, to the perplexing calculations behind Shortest Path Problems. It's a place for quick reference and
+                intensive learning for both beginnings and experts alike.
+            </p>
 
-        <aside>
-            <a href="http://simon-huang.ca" target="_blank">Simon Huang</a>
-            |
-            <a href="http://ayoungprogrammer.com" target="_blank">Michael Young</a>
-        </aside>
+            <p> Again, welcome and enjoy your stay!</p>
+            <p> - Simon and Michael</p>
+
+            <br/>
+
+
+            <h3>check out our profiles</h3>
+
+            <aside>
+                <a href="http://simon-huang.ca" target="_blank">Simon Huang</a>
+                |
+                <a href="http://ayoungprogrammer.com" target="_blank">Michael Young</a>
+            </aside>
         </article>
     </main>
 </section>
