d2253308d33f491c9be879053ecbb33f1ae279a3
simonhuang989 <simonhuang989@hotmail.com>
2014-03-21 20:59:45 -0400

    table responsiveness added

--- templates/listFooter.php
+++ templates/listFooter.php
@@ -25,6 +25,31 @@
     prettyPrint();
 </script>
 
+<script>
+    function resizeTable (){
+        var windowWidth = $(window).width();
+        var table = $('section table');
+        if (windowWidth<700){
+            if (!table.parent().is('div')){
+                $('section table').wrap('<div></div>');
+            }
+            var tableWidth = $(window).width() - 70;
+            console.log(tableWidth);
+            $('section div').css('width', tableWidth);
+        } else {
+            if (!table.parent().is('div')){
+                //$('section table').unwrap('<div></div>');
+            }
+        }
+    }
+    (function(){
+        resizeTable();
+    })();
+    $(window).resize(function(){
+        resizeTable();
+    });
+</script>
+
 <script type="text/javascript" src="./public_html/js/side_nav.js"> </script>
 <script type="text/javascript" src="./public_html/js/responsive_nav.js"></script>
 <script type="text/javascript" src="./public_html/js/nested_nav.js"></script>
