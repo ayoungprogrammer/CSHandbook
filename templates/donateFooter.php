a009e3303c9e8701b34f83654dac1b121dca9e34
simonhuang989 <simonhuang989@hotmail.com>
2014-03-26 16:56:49 -0400

    sorting algs

--- templates/donateFooter.php
+++ templates/donateFooter.php
@@ -22,39 +22,31 @@
         return false;
     }
 
-    // Assumes jQuery is loaded!
-    // Watch for the document to be ready:
     $(document).ready(function() {
         // Watch for a form submission:
         $("#payment-form").submit(function(event) {
-
             // Flag variable:
             var error = false;
-
             // disable the submit button to prevent repeated clicks:
             $('#submitBtn').attr("disabled", "disabled");
-
             // Get the values:
-            var ccNum = $('.card-number').val(), cvcNum = $('.card-cvc').val(), expMonth = $('.card-expiry-month').val(), expYear = $('.card-expiry-year').val();
+            var ccNum = $('.card-number').val(),
+                cvcNum = $('.card-cvc').val(),
+                expMonth = $('.card-expiry-month').val(),
+                expYear = $('.card-expiry-year').val();
 
-            // Validate the number:
             if (!Stripe.validateCardNumber(ccNum)) {
                 error = true;
                 reportError('The credit card number appears to be invalid.');
             }
-
-            // Validate the CVC:
             if (!Stripe.validateCVC(cvcNum)) {
                 error = true;
                 reportError('The CVC number appears to be invalid.');
             }
-
-            // Validate the expiration:
             if (!Stripe.validateExpiry(expMonth, expYear)) {
                 error = true;
                 reportError('The expiration date appears to be invalid.');
             }
-
             // Validate other form elements, if needed!
 
             // Check for errors:
@@ -69,37 +61,26 @@
                 }, stripeResponseHandler);
 
             }
-
             // Prevent the form from submitting:
             return false;
-
         }); // Form submission
-
     }); // Document ready.
 
     // Function handles the Stripe response:
     function stripeResponseHandler(status, response) {
-
         // Check for an error:
         if (response.error) {
-
             reportError(response.error.message);
-
         } else { // No errors, submit the form:
-
             var f = $("#payment-form");
-
             // Token contains id, last4, and card type:
             var token = response['id'];
 
             // Insert the token into the form so it gets submitted to the server
             f.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
-
             // Submit the form:
             f.get(0).submit();
-
         }
-
     } // End of stripeResponseHandler() function.
 </script>
 
